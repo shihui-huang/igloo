4913b339e71a1b81bf634a481d0c5758e2a97f73
Paul Mabileau <paulmabileau@hotmail.fr>
2020-03-04 17:38:27 +0100

    Documented Chambre
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- Code/src/main/java/eu/telecomsudparis/csc4102/gestionclefshotel/Chambre.java
+++ Code/src/main/java/eu/telecomsudparis/csc4102/gestionclefshotel/Chambre.java
@@ -3,6 +3,18 @@ package eu.telecomsudparis.csc4102.gestionclefshotel;
 import eu.telecomsudparis.csc4102.gestionclefshotel.exception.ProblemeDansGenerationClef;
 
 
+/**
+ * Classe représentant une chambre dans le système de
+ * gestion des clefs d'un hôtel. D'un point de vue modèle,
+ * elle est reliée bi-directionnellement à {@link Badge}
+ * et uni-directionnellement à {@link PaireClefs}.
+ * 
+ * @see Badge
+ * @see Client
+ * @see PaireClefs
+ * @see GestionClefsHotel
+ * @author Paul Mabileau
+ */
 public class Chambre {					// TODO Documentation.
 	private final long id;
 	private final String graine;
@@ -11,6 +23,18 @@ public class Chambre {					// TODO Documentation.
 	private Badge badge;
 	private PaireClefs paireClefs;
 	
+	/**
+	 * Construit une nouvelle chambre et initialise sa paire
+	 * de clés à partir des paramètres fournis.
+	 * 
+	 * @param id L'identifiant que va prendre la chambre.
+	 * @param graine La graine servant aux générations de
+	 *		paires de clés.
+	 * @param sel Le sel servant aux générations de paires
+	 *		de clés qui va être incrémentés à chaque fois.
+	 * @throws ProblemeDansGenerationClef Proviens de la
+	 *		génération de clés réalisée par {@link Util}.
+	 */
 	public Chambre(final long id, final String graine, final int sel) throws ProblemeDansGenerationClef {
 		this.id = id;
 		this.graine = graine;
@@ -28,18 +52,42 @@ public class Chambre {					// TODO Documentation.
 		return false;					// TODO invariant.
 	}
 	
+	/**
+	 * @return L'identifiant de la chambre.
+	 */
 	public long getId() {
 		return this.id;
 	}
 	
+	/**
+	 * @return Le badge auquel est potentiellement associé
+	 *			la chambre. Peut être {@code null}.
+	 */
 	public Badge getBadge() {
 		return this.badge;
 	}
 	
+	/**
+	 * Associe le badge fourni à la chambre sans associer
+	 * la chambre au badge, c'est donc unidirectionnel.
+	 * 
+	 * @param badge Le badge à associer.
+	 * @see #associerBadge(Badge, boolean)
+	 */
 	public void associerBadge(final Badge badge) {
 		this.associerBadge(badge, false);
 	}
 	
+	/**
+	 * Associe le badge fourni à la chambre donnée et
+	 * vice-versa si {@code bidirectionnel} vaut {@code
+	 * true}.
+	 * 
+	 * @param badge Le badge à associer.
+	 * @param bidirectionnel S'il faut aussi associer la
+	 *		chambre au badge.
+	 * @see #associerBadge(Badge)
+	 */
 	public void associerBadge(final Badge badge, boolean bidirectionnel) {
 		this.badge = badge;
 		
@@ -48,10 +96,24 @@ public class Chambre {					// TODO Documentation.
 		}
 	}
 	
+	/**
+	 * Rompt le lien d'association de la chambre vers son
+	 * badge sans en faire de même pour la direction opposée.
+	 * 
+	 * @see #dissocierBadge(boolean)
+	 */
 	public void dissocierBadge() {
 		this.dissocierBadge(false);
 	}
 	
+	/**
+	 * Rompt le lien d'association de la chambre vers son
+	 * badge en en faisant de même pour la direction opposée.
+	 * 
+	 * @param bidirectionnel S'il faut aussi dissocier dans
+	 *		l'autre sens.
+	 * @see #dissocierBadge()
+	 */
 	public void dissocierBadge(boolean bidirectionnel) {
 		if (bidirectionnel && this.badge != null) {
 			this.badge.dissocierClient(false);
@@ -60,22 +122,44 @@ public class Chambre {					// TODO Documentation.
 		this.badge = null;
 	}
 	
+	/**
+	 * @return La graine de génération de clés de la chambre.
+	 */
 	public String getGraine() {
 		return this.graine;				// TODO Return clone instead.
 	}
 	
+	/**
+	 * @return La sel de génération de clés de la chambre.
+	 */
 	public int getSel() {
 		return this.sel;
 	}
 	
+	/**
+	 * @return Si la chambre est occupée ou non.
+	 */
 	public boolean estOccupee() {
 		return this.occupee;
 	}
 	
+	/**
+	 * @return La {@link PaireClefs} de la chambre.
+	 */
 	public PaireClefs getClefs() {
 		return this.paireClefs;
 	}
 	
+	/**
+	 * Génère une nouvelle {@link PaireClefs} pour la chambre
+	 * en mettant l'ancienne clef n°2 comme nouvelle clef n°1
+	 * et en générant une nouvelle clef n°2 à partir des membres
+	 * {@link #graine} et {@link #sel} en incrémentant ce dernier.
+	 * 
+	 * @return La nouvelle paire ainsi générée.
+	 * @throws ProblemeDansGenerationClef Si la génération
+	 *		s'est mal produite.
+	 */
 	public PaireClefs obtenirNouvellePaireClefs() throws ProblemeDansGenerationClef {
 		this.paireClefs = new PaireClefs(this.paireClefs.getClef1(),
 										Util.genererUneNouvelleClef(this.graine,
@@ -84,15 +168,31 @@ public class Chambre {					// TODO Documentation.
 		return this.paireClefs;
 	}
 	
+	/**
+	 * Enregistre la paire de clefs données dans la chambre.
+	 * @param paireClefs L'objet {@link PaireClefs} à stocker.
+	 */
 	public void inscrireClefs(final PaireClefs paireClefs) {
 		this.paireClefs = paireClefs;
 	}
 	
+	/**
+	 * Libère la chambre de son client en le dissociant du
+	 * badge associé à la chambre (bidirectionnellement).
+	 * Maintient le lien entre la chambre et son badge. Marque
+	 * la chambre comme non occupée.
+	 */
 	public void liberer() {
 		this.occupee = false;
 		this.badge.dissocierClient(true);
 	}
 	
+	/**
+	 * Implémentation de hashCode() pour {@link Chambre}
+	 * basée sur les membres {@link #id}, {@link #graine}
+	 * et {@link #sel}.
+	 * <br><br>{@inheritDoc}
+	 */
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -103,6 +203,13 @@ public class Chambre {					// TODO Documentation.
 		return result;
 	}
 	
+	/**
+	 * Implémentation de equals() pour {@link Chambre} pour
+	 * laquelle l'égalité est basée sur les membres {@link
+	 * #id}, {@link #graine} et {@link #sel}.
+	 * <br><br>{@inheritDoc}
+	 * @return Si la chambre est égale à l'objet donné.
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj) {
@@ -135,6 +242,11 @@ public class Chambre {					// TODO Documentation.
 		return true;
 	}
 	
+	/**
+	 * Implémentation de toString() pour {@link Chambre}.
+	 * <br><br>{@inheritDoc}
+	 * @return Une représentation textuelle de la chambre.
+	 */
 	@Override
 	public String toString() {
 		return String.format("Chambre [id = %s, graine = %s, sel = %s, occupee = %s, paireClefs = %s]",
