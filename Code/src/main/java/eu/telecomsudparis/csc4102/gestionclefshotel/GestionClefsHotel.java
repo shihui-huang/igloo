7cd2c564524bd5710d849e8bda35d55d6f4a185e
Paul Mabileau <paulmabileau@hotmail.fr>
2020-03-04 22:51:39 +0100

    Documented GestionClefsHotel
    The methods left undocumented are either unimplemented or unsure.
    
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- Code/src/main/java/eu/telecomsudparis/csc4102/gestionclefshotel/GestionClefsHotel.java
+++ Code/src/main/java/eu/telecomsudparis/csc4102/gestionclefshotel/GestionClefsHotel.java
@@ -7,15 +7,28 @@ import eu.telecomsudparis.csc4102.gestionclefshotel.exception.ProblemeDansGenera
 
 
 /**
- * Cette classe définit la façade du système de gestion des clefs de l'hôtel.
+ * Cette classe définit la façade du système de gestion des
+ * clefs de l'hôtel.
  */
 public class GestionClefsHotel {								// TODO Documentation.
+	/**
+	 * La collection de chambres de l'hôtel indexée par leurs
+	 * identifiants supposés uniques.
+	 */
 	private final HashMap<Long, Chambre> chambres;
+	/**
+	 * La collection de badges de l'hôtel indexée par leurs
+	 * identifiants supposés uniques.
+	 */
 	private final HashMap<Long, Badge> badges;
+	/**
+	 * La collection de clients de l'hôtel indexée par leurs
+	 * identifiants supposés uniques.
+	 */
 	private final HashMap<Long, Client> clients;
 	
 	/**
-	 * Construit la façade.
+	 * Construit la façade. Initialise les collections.
 	 */
 	public GestionClefsHotel() {
 		this.chambres = new HashMap<Long, Chambre>();
@@ -27,12 +40,28 @@ public class GestionClefsHotel {								// TODO Documentation.
 	
 	/**
 	 * Teste si l'invariant est vérifié.
-	 * @return true si l'invariant est vérifié.
+	 * @return {@code true} si l'invariant est vérifié.
 	 */
 	public boolean invariant() {
 		return true;											// TODO invariant.
 	}
 	
+	/**
+	 * Opération cas d'utilisation "Créer une chambre" : la
+	 * façade reçoit un identifiant, une graine et un sel
+	 * et crée une nouvelle chambre grâce à ceux-ci, si l'
+	 * identifiant n'est pas déjà utilisé et que la graine
+	 * est non vide.
+	 * 
+	 * @param id L'identifiant de la chambre à créer.
+	 * @param graine La graine de génération de clés de la
+	 *		chambre.
+	 * @param sel Le sel de génération de clés de la chambre.
+	 * @return La nouvelle chambre si l'identifiant est libre,
+	 *		l'ancienne sinon.
+	 * @throws ProblemeDansGenerationClef Si la génération
+	 *		est cassée en mille morceaux.
+	 */
 	public Chambre creerChambre(final long id, final String graine, final int sel)
 			throws ProblemeDansGenerationClef {
 		final Chambre current = this.chercherChambre(id);
@@ -47,14 +76,40 @@ public class GestionClefsHotel {								// TODO Documentation.
 		}
 	}
 	
+	/**
+	 * Cherche et renvoie la chambre associée à l'identifiant
+	 * donné. Si aucune chambre n'a cet identifiant, {@code null}
+	 * est alors renvoyé.
+	 * 
+	 * @param id L'identifiant de la chambre à chercher.
+	 * @return La chambre associé à l'id, {@code null} sinon.
+	 */
 	public Chambre chercherChambre(final long id) {
 		return this.chambres.get(id);
 	}
 	
+	/**
+	 * @return Une collection des chambres de l'hôtel.
+	 */
 	public Collection<Chambre> listerChambres() {
 		return this.chambres.values();
 	}
 	
+	/**
+	 * Opération cas d'utilisation "Enregistrer l'occupation
+	 * d'une chambre par un client". Cherche la chambre, le
+	 * badge et le client associés aux identifiants fournis, et
+	 * si ceux-ci réunissent les conditions nécessaires, le badge
+	 * va alors être associé bidirectionnellement au client et
+	 * de même à la chambre qui obtient une nouvelle paire de
+	 * clefs enfin inscrite dans le badge.
+	 * 
+	 * @param idChambre La chambre à faire occuper par le client.
+	 * @param idBadge Le badge qui va servir à ouvrir la chambre.
+	 * @param idClient Le client payant pour la chambre.
+	 * @throws ProblemeDansGenerationClef Si la génération
+	 *		est cassée en mille morceaux.
+	 */
 	public void enregistrerOccupationChambre(final long idChambre,
 											final long idBadge,
 											final long idClient) throws ProblemeDansGenerationClef {
@@ -62,19 +117,30 @@ public class GestionClefsHotel {								// TODO Documentation.
 		final Badge badge = this.chercherBadge(idBadge);
 		final Client client = this.chercherClient(idClient);
 		
-		if (chambre != null && client != null && badge != null && badge.getClefs() == null) {
+		if (badge != null && badge.getClefs() == null
+				&& chambre != null && client != null) {
 			badge.associerClient(client, true);
 			badge.associerChambre(chambre, true);
 			badge.inscrireClefs(chambre.obtenirNouvellePaireClefs());
 		}
 	}
 	
+	/**
+	 * Opération cas d'utilisation "Libérer une chambre". Cherche
+	 * la chambre et le badge potentiellement associés aux IDs
+	 * fournis et s'il respectent certaines conditions, la chambre
+	 * est alors libérée avec {@link Chambre#liberer()} et le badge
+	 * est vidé de ses clefs avec {@link Badge#vider()}.
+	 * 
+	 * @param idChambre La chambre à libérer de son client.
+	 * @param idBadge Le badge du client.
+	 */
 	public void libererChambre(final long idChambre, final long idBadge) {
 		final Chambre chambre = this.chercherChambre(idChambre);
 		final Badge badge = this.chercherBadge(idBadge);
 		
 		if (chambre != null && badge != null && badge.getClefs() == null) {
-			chambre.liberer();
+			chambre.liberer();									// TODO Check condition.
 			badge.vider();
 		}
 	}
@@ -85,6 +151,14 @@ public class GestionClefsHotel {								// TODO Documentation.
 		return badge;
 	}
 	
+	/**
+	 * Cherche et renvoie le badge associée à l'identifiant
+	 * donné. Si aucun badge n'a cet identifiant, {@code null}
+	 * est alors renvoyé.
+	 * 
+	 * @param id L'identifiant du badge à chercher.
+	 * @return Le badge associé à l'id, {@code null} sinon.
+	 */
 	public Badge chercherBadge(final long id) {
 		return this.badges.get(id);
 	}
@@ -95,6 +169,14 @@ public class GestionClefsHotel {								// TODO Documentation.
 		return client;
 	}
 	
+	/**
+	 * Cherche et renvoie le client associée à l'identifiant
+	 * donné. Si aucun client n'a cet identifiant, {@code null}
+	 * est alors renvoyé.
+	 * 
+	 * @param id L'identifiant du client à chercher.
+	 * @return Le client associé à l'id, {@code null} sinon.
+	 */
 	public Client chercherClient(final long id) {
 		return this.clients.get(id);
 	}
