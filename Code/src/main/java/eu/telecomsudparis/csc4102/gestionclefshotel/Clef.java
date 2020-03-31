e1b2cae0c5b3b774eaa86a9cec012a3964cb81ef
Paul Mabileau <paulmabileau@hotmail.fr>
2020-03-31 22:19:29 +0200

    Fix presentation
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- Code/src/main/java/eu/telecomsudparis/csc4102/gestionclefshotel/Clef.java
+++ Code/src/main/java/eu/telecomsudparis/csc4102/gestionclefshotel/Clef.java
@@ -1,64 +1,66 @@
 package eu.telecomsudparis.csc4102.gestionclefshotel;
 
-import eu.telecomsudparis.csc4102.gestionclefshotel.exception.ProblemeDansGenerationClef;
-
 import java.util.Arrays;
 
+import eu.telecomsudparis.csc4102.gestionclefshotel.exception.ProblemeDansGenerationClef;
+
+
 public class Clef {
-    /**
-     * la clef.
-     */
-    private byte[] value;
-
-
-    /**
-     * Instantiates a new Clef.
-     *
-     * @param value the value
-     * @throws ProblemeDansGenerationClef the probleme dans generation clef
-     */
-    public Clef(final byte[] value) {
-        this.value = value.clone();
-    }
-
-    /**
-     * Instantiates a new Clef.
-     */
-    public Clef() {
-        this.value = new byte[Util.TAILLE_CLEF];
-    }
-
-    public Clef(final Clef clef) {
-        this.value = clef.value.clone();
-    }
-    /**
-     * Get clef byte [ ].
-     *
-     * @return the byte [ ]
-     */
-    public byte[] getValue() {
-        return value.clone();
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Clef clef = (Clef) o;
-        return Arrays.equals(value, clef.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Arrays.hashCode(value);
-    }
-
-    @Override
-    public String toString() {
-        return Util.clefToString(value);
-    }
+	/**
+	 * La clef.
+	 */
+	private byte[] value;
+	
+	public Clef(final Clef clef) {
+		this.value = clef.value.clone();
+	}
+	
+	/**
+	 * Instantiates a new Clef.
+	 *
+	 * @param  value                      the value
+	 * @throws ProblemeDansGenerationClef the probleme dans generation clef
+	 */
+	public Clef(final byte[] value) {
+		this.value = value.clone();
+	}
+	
+	/**
+	 * Instantiates a new Clef.
+	 */
+	public Clef() {
+		this.value = new byte[Util.TAILLE_CLEF];
+	}
+	
+	/**
+	 * Get clef byte [ ].
+	 *
+	 * @return the byte [ ]
+	 */
+	public byte[] getValue() {
+		return this.value.clone();
+	}
+	
+	@Override
+	public boolean equals(final Object o) {
+		if (this == o) {
+			return true;
+		}
+		
+		if (o == null || this.getClass() != o.getClass()) {
+			return false;
+		}
+		
+		return Arrays.equals(this.value, ((Clef) o).value);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Arrays.hashCode(this.value);
+	}
+	
+	@Override
+	public String toString() {
+		return Util.clefToString(this.value);
+	}
 }
--- Code/src/main/java/eu/telecomsudparis/csc4102/gestionclefshotel/PaireClefs.java
+++ Code/src/main/java/eu/telecomsudparis/csc4102/gestionclefshotel/PaireClefs.java
@@ -1,8 +1,8 @@
 package eu.telecomsudparis.csc4102.gestionclefshotel;
 
-import eu.telecomsudparis.csc4102.gestionclefshotel.exception.ProblemeDansGenerationClef;
-
 import java.util.Arrays;
+
+import eu.telecomsudparis.csc4102.gestionclefshotel.exception.ProblemeDansGenerationClef;
 
 
 /**
@@ -10,9 +10,9 @@ import java.util.Arrays;
  * déverrouiller des serrures.
  *
  * @author Paul Mabileau
- * @see Chambre
- * @see Badge
- * @see GestionClefsHotel
+ * @see    Chambre
+ * @see    Badge
+ * @see    GestionClefsHotel
  */
 public class PaireClefs {
 	/**
@@ -23,13 +23,12 @@ public class PaireClefs {
 	 * La deuxième clef de la paire.
 	 */
 	private Clef clef2;
-
-
+	
 	/**
 	 * Instantiates a new Paire clefs.
 	 *
-	 * @param clef1 the clef 1
-	 * @param clef2 the clef 2
+	 * @param  clef1                      the clef 1
+	 * @param  clef2                      the clef 2
 	 * @throws ProblemeDansGenerationClef the probleme dans generation clef
 	 */
 	public PaireClefs(final Clef clef1, final Clef clef2) {
@@ -37,6 +36,7 @@ public class PaireClefs {
 		this.clef2 = clef2;
 		assert this.invariant();
 	}
+	
 	/**
 	 * Invariant boolean.
 	 *
@@ -46,19 +46,21 @@ public class PaireClefs {
 		return this.clef1 != null && this.clef1.getValue().length == Util.TAILLE_CLEF
 				&& this.clef2 != null && this.clef2.getValue().length == Util.TAILLE_CLEF;
 	}
-
+	
 	/**
 	 * @return Une copie de la première clef.
 	 */
-	public Clef getClef1() { return clef1; }
-
+	public Clef getClef1() {
+		return this.clef1;
+	}
+	
 	/**
 	 * @return Une copie de la deuxième clef.
 	 */
 	public Clef getClef2() {
-		return clef2;
+		return this.clef2;
 	}
-
+	
 	/**
 	 * Implémentation de hashCode() pour {@link PaireClefs} basée sur les deux
 	 * clefs de la paire. <br>
@@ -73,7 +75,7 @@ public class PaireClefs {
 		result = prime * result + Arrays.hashCode(this.clef2.getValue());
 		return result;
 	}
-
+	
 	/**
 	 * Implémentation de equals() pour {@link PaireClefs} basée sur les deux
 	 * clefs de la paire. <br>
@@ -85,24 +87,24 @@ public class PaireClefs {
 		if (this == obj) {
 			return true;
 		}
-
+		
 		if (!(obj instanceof PaireClefs)) {
 			return false;
 		}
-
+		
 		final PaireClefs other = (PaireClefs) obj;
-
+		
 		if (!Arrays.equals(this.clef1.getValue(), other.clef1.getValue())) {
 			return false;
 		}
-
+		
 		if (!Arrays.equals(this.clef2.getValue(), other.clef2.getValue())) {
 			return false;
 		}
-
+		
 		return true;
 	}
-
+	
 	/**
 	 * Implémentation de toString() pour {@link PaireClefs} basée sur les deux
 	 * clefs de la paire. <br>
@@ -111,7 +113,8 @@ public class PaireClefs {
 	 */
 	@Override
 	public String toString() {
-	return String.format("PaireClefs [clef1 = %s, clef2 = %s]",
-						Util.clefToString(this.clef1.getValue()), Util.clefToString(this.clef2.getValue()));
-}
+		return String.format("PaireClefs [clef1 = %s, clef2 = %s]",
+							Util.clefToString(this.clef1.getValue()),
+							Util.clefToString(this.clef2.getValue()));
+	}
 }
